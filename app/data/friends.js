442282430644fa5ccdd65c958371ac86ba02c1ae
Ilham Hafizovic <ilham.hafizovic@gmail.com>
2018-11-04 22:36:23 -0500

    Added more data to the friends.js file.

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,13 +6,45 @@ var friendsArray = [
       1,
       3,
       4,
-      7,
-      9,
-      10,
+      5,
+      5,
+      1,
       4,
       4,
       2,
       1
+    ]
+  },
+  {
+    name: "Luke Skywalker",
+    photo: "https://i.pinimg.com/originals/40/d6/1d/40d61dde8f4aced9ecd96269b5050ebe.jpg",
+    scores: [
+      3,
+      5,
+      5,
+      4,
+      3,
+      2,
+      1,
+      5,
+      2,
+      1
+    ]
+  },
+  {
+    name: "",
+    photo: "https://images-na.ssl-images-amazon.com/images/I/41trnBjzshL.jpg",
+    scores: [
+      5,
+      1,
+      1,
+      2,
+      3,
+      5,
+      5,
+      1,
+      2,
+      5
     ]
   }
 ];
